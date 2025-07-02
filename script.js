5a835172f80d354b1b188fe8362ae0c329c11934
Quang <nguyenphuquang1998@gmail.com>
2025-07-02 13:44:48 +0700

    Update

---
+++ script.js
@@ -0,0 +1,15 @@
+$(function () {
+
+    $('.nav-link').click(function () {
+        var id = $(this).attr('data-tabid');
+        $('.nav-link').removeClass('active');
+
+        $('#content-tab').html('');
+        if (id) {
+            $(this).addClass('active');
+            $.get('tab' + id + '.html', function (data) {
+                $('#content-tab').html(data);
+            });
+        }
+    });
+});
