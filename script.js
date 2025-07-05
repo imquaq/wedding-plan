bf0d411d3dbf427e0c3a89595cfc7daa129e4603
Quang <nguyenphuquang1998@gmail.com>
2025-07-05 16:22:02 +0700

    Update

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 $(function () {
+    $('.nav-link').first().addClass('active');
+    $.get('tab1.html', function (data) {
+        $('#content-tab').html(data);
+    });
 
     $('.nav-link').click(function () {
         var id = $(this).attr('data-tabid');
-        $('.nav-link').removeClass('active');
+        // $('.nav-item').removeClass('active');
+        $('.nav-link').removeClass('active')
 
         $('#content-tab').html('');
         if (id) {
             $(this).addClass('active');
+            // $('#navItem' + id).addClass('active');
             $.get('tab' + id + '.html', function (data) {
                 $('#content-tab').html(data);
             });
         }
     });
+
+    // $('.nav-link').click(function () {
+    //     var id = $(this).attr('data-tabid');
+    //     $('.nav-link').removeClass('active');
+
+    //     $('#content-tab').html('');
+    //     if (id) {
+    //         $(this).addClass('active');
+    //         $.get('tab' + id + '.html', function (data) {
+    //             $('#content-tab').html(data);
+    //         });
+    //     }
+    // });
 });
