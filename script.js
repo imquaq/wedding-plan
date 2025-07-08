ac61ee0a371b79fab837ead160a18e5ce99cb770
Quang <nguyenphuquang1998@gmail.com>
2025-07-08 22:17:24 +0700

    Update

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 $(function () {
-    $('#div-login').show();
-    $('#div-content').hide();
-
-    $('.nav-link').first().addClass('active');
+    var user = getCookieWithExpiry("user");
+    if (user && user !== "") {
+        $('#div-login').hide();
+        $('#div-content').show();
+    } else {
+        $('#div-login').show();
+        $('#div-content').hide();
+    }
+    $('.tab-main').first().addClass('active');
     $.get('tab1.html', function (data) {
         $('#content-tab').html(data);
     });
 
-    $('.nav-link').click(function () {
+    $('.tab-main').click(function () {
         var id = $(this).attr('data-tabid');
         // $('.nav-item').removeClass('active');
-        $('.nav-link').removeClass('active')
+        $('.tab-main').removeClass('active')
 
         $('#content-tab').html('');
         if (id) {
@@ -31,7 +36,6 @@ $(function () {
         }
     });
 
-
     function login() {
         var password = $('#txtPassword').val();
         if (password !== "") {
@@ -44,7 +48,52 @@ $(function () {
             if (listPass.includes(password.toLowerCase())) {
                 $('#div-login').hide();
                 $('#div-content').show();
+                setCookieWithExpiry("user", password, 3);
             }
         }
+    }
+
+    function setCookie(name, value, hour) {
+        let expires = "";
+        if (hour) {
+            const date = new Date();
+            date.setTime(date.getTime() + (hour * 60 * 60 * 1000));
+            console.log(date);
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + encodeURIComponent(value) + expires + "; path=/";
+    }
+
+    function getCookie(name) {
+        const cookies = document.cookie.split("; ");
+        for (let c of cookies) {
+            const [key, value] = c.split("=");
+            if (key === name) return decodeURIComponent(value);
+        }
+        return null;
+    }
+
+    function setCookieWithExpiry(name, value, hour) {
+        const now = new Date();
+        const expiryTimestamp = now.getTime() + hour * 60 * 60 * 1000;
+        const data = JSON.stringify({ value, expiry: expiryTimestamp });
+
+        document.cookie = name + "=" + encodeURIComponent(data) + "; path=/";
+    }
+
+    function getCookieWithExpiry(name) {
+        const cookies = document.cookie.split("; ");
+        for (let c of cookies) {
+            const [key, val] = c.split("=");
+            if (key === name) {
+                const data = JSON.parse(decodeURIComponent(val));
+                if (Date.now() < data.expiry) {
+                    return data.value; // Còn hiệu lực
+                } else {
+                    return null; // Hết hạn
+                }
+            }
+        }
+        return null; // Không tồn tại
     }
 });
